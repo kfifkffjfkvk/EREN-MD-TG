d9d2824f51235bde352ef3a4326c09c7330fd09f
kfifkffjfkvk <toxickichuxrestapi@gmail.com>
2024-07-01 20:07:34 +0530

    fixed?

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 const BOT_TOKEN = process.env.BOT_TOKEN || '7244703646:AAGHnZQa4UXwOFzpyi_vsnCJ6ZPMYfCE3Ic';
+const MONGO_DB_URI = process.env.MONGO_DB_URI || 'mongodb+srv://kawshik:clashwabot@clashwabot.m7bjy65.mongodb.net/?retryWrites=true&w=majority';
 
 module.exports = {
-  BOT_TOKEN
+  BOT_TOKEN,
+  MONGO_DB_URI
 };
