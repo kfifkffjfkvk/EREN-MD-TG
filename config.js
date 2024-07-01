b87b979d5723f029b8cfff913a0ea2d6e97d96c3
kfifkffjfkvk <toxickichuxrestapi@gmail.com>
2024-07-01 21:46:43 +0530

    fixed?

--- config.js
+++ config.js
@@ -1,7 +1,9 @@
 const BOT_TOKEN = process.env.BOT_TOKEN || '7244703646:AAGHnZQa4UXwOFzpyi_vsnCJ6ZPMYfCE3Ic';
 const MONGO_DB_URI = process.env.MONGO_DB_URI || 'mongodb+srv://kawshik:clashwabot@clashwabot.m7bjy65.mongodb.net/?retryWrites=true&w=majority';
+const START_IMG = process.env.START_IMG || 'https://i.imgur.com/mZYqXPx.jpeg';
 
 module.exports = {
   BOT_TOKEN,
-  MONGO_DB_URI
+  MONGO_DB_URI,
+  START_IMG
 };
