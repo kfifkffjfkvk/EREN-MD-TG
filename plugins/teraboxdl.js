6d2564b114ed1414bfc06aba7820d0d29003acfa
kfifkffjfkvk <toxickichuxrestapi@gmail.com>
2024-08-04 15:20:49 +0530

    fixed?

--- plugins/teraboxdl.js
+++ plugins/teraboxdl.js
@@ -9,10 +9,8 @@ cmd({
     if (args.startsWith("https://terasharelink.com/")) {
       try {
       await m.reply("Downloading...", { reply_to_message_id : m.message.message_id });
-      console.log(args);
       let data = await axios.get(`https://terabox.udayscriptsx.workers.dev/?url=${args}`);
-      let result = await data.data.response[0].resolutions['Fast Download'];
-      console.log(result);
+      let result = data.data.response[0].resolutions['Fast Download'];
       let buff = await (await fetch(result)).buffer();
       return await m.replyWithVideo({ source: buff },{ reply_to_message_id : m.message.message_id } );
       } catch (e) {
